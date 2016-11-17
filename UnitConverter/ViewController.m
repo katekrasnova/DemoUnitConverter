a5b6741506aeeb65a18d93722406dbb1b4a141b8
katekrasnova <katekrasnova1990@gmail.com>
2016-11-17 10:50:42 +0300

    DemoUnitConverter

--- UnitConverter/ViewController.m
+++ UnitConverter/ViewController.m
@@ -8,11 +8,52 @@
 
 #import "ViewController.h"
 
+double convertKilometersToMiles(double unitInKilometers) {
+    double unitInMiles;
+    unitInMiles = 0.62 * unitInKilometers;
+    return unitInMiles;
+}
+
+double convertKilometersToYards(double unitInKilometers) {
+    double unitInYards;
+    unitInYards = 1093.61 * unitInKilometers;
+    return unitInYards;
+}
+
+double convertKilometersInFeet(double unitInKilometers) {
+    double unitInFeet;
+    unitInFeet = 3280.84 * unitInKilometers;
+    return unitInFeet;
+}
+
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *inputField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentController;
+@property (weak, nonatomic) IBOutlet UILabel *outputField;
 
 @end
 
 @implementation ViewController
+
+- (IBAction)updateButton:(id)sender {
+    
+    NSMutableString *buf = [NSMutableString new];
+    double userInput = [self.inputField.text doubleValue];
+    
+    if (self.segmentController.selectedSegmentIndex == 0) {
+        double unitInMiles = convertKilometersToMiles(userInput);
+        [buf appendString:[@(unitInMiles) stringValue]];
+    }
+    else if (self.segmentController.selectedSegmentIndex == 1) {
+        double unitInYards = convertKilometersToYards(userInput);
+        [buf appendString:[@(unitInYards) stringValue]];
+    }
+    else {
+        double unitInFeet = convertKilometersInFeet(userInput);
+        [buf appendString:[@(unitInFeet) stringValue]];
+    }
+    self.outputField.text = buf;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
